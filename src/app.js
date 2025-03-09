8049f7d7b8cf9c3efff5bb8ae010242efde6d477
Dimpal Gogoi <dimpalgogoi@Dimpals-MacBook-Air.local>
2025-03-09 15:04:15 +0530

    using of middleware auth in profile and also add token expire time and coockies expire time

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const bcrypt=require("bcrypt");
 const cookieParser=require("cookie-parser");
 const jwt=require("jsonwebtoken");
 const {userAuth}=require("./middlewares/auth");
+const { data } = require("react-router-dom");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -53,10 +54,15 @@ app.post("/login",async(req,res)=>{
         if(isStrongPassword){
             //create JWT token
 
-            const token=await jwt.sign({_id:user._id},"DEV@tinder$790");
+            const token=await jwt.sign({_id:user._id},"DEV@tinder$790",{
+                expiresIn: "7d",
+            });
 
             //Add the token to cookie and send the response back to user 
-            res.cookie("token",token);
+            res.cookie("token",token,{
+                expires:new Date(Date.now()+1*90000000),
+            });
+
             res.send("login succesfull");
         }else{
             throw new Error("Invalid UserId or Password")
@@ -78,11 +84,19 @@ app.get("/profile",userAuth,async(req,res)=>{
     }
 });
 
+app.post("/sendConnectionRequest",userAuth,async(req,res)=>{
 
+    const user =req.user;
+    //sending a connection requsest
+    console.log("sending connection req ");
+
+    res.send(user.firstName+ " is sending connection requset");
+
+})
 
 connectDB()
     .then(()=>{
-        console.log("database connection eastablish.....");
+        console.log("database connection eascotablish.....");
         app.listen(777,()=>{
             console.log("server is sucessfully listening to port no '777'");
         });
