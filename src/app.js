7619d2449e33fdb06bc475091fcffcd9483ac5ce
Dimpal Gogoi <dimpalgogoi@Dimpals-MacBook-Air.local>
2025-03-09 14:26:49 +0530

    delete the api calls like feed,user,.....etc

--- src/app.js
+++ src/app.js
@@ -78,74 +78,7 @@ app.get("/profile",userAuth,async(req,res)=>{
     }
 });
 
-app.get("/feed",async(req,res)=>{
-    try{
-        const users=await User.find({});
-        res.send(users);
-    }
-    catch(err){
-        res.status(404).send("something went wrong...");
-    }
-})
 
-app.get("/users",async(req,res)=>{
-    const userEmail=req.body.emailId;
-    try {
-        const users=await User.find({emailId:userEmail});
-        if(users.length===0){
-            res.status(400).send("user not found !");
-        }else{
-            res.send(users);
-        }
-        
-    } catch (error) {
-        res.status(404).send("somthing went wrong");
-        
-    }
-})
-
-app.delete("/user",async(req,res)=>{
-    const userID=req.body.userID;
-    try {
-        const user=await User.findByIdAndDelete({ _id:userID});
-        res.send("deleted Succesfully");
-        
-    } catch (err) {
-        res.status(400).send("somthing went Wrong");
-        
-    }
-
-})
-
-app.patch("/user/:userId",async(req,res)=>{
-    const userId=req.params?.userId;
-    const data=req.body;
-    try {
-        const ALLOW_UPDATES=["age","gender","about","skills"];
-        const isUpadateallowed=Object.keys(data).every((k)=>
-        ALLOW_UPDATES.includes(k),
-        );
-        if(!isUpadateallowed){
-            throw new Error("Update not allowed !");
-        }
-        if(req.body.skills.length>10){
-            throw new Error("Skills More than 10 Not allowed !");
-        }
-        if(data.about&&data.about.length>100){
-            throw new Error("More than 100 word not allowed Not allowed !");
-        }
-        await User.findByIdAndUpdate({ _id: userId },data,
-            {
-                returnDocument:"before",
-                runValidators:true,
-            });
-        res.send("Upadated successfully !!");
-        
-    } catch (error) {
-        res.status(400).send("something went wrong : "+error.message);
-    }
-
-})
 
 connectDB()
     .then(()=>{
